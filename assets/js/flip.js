50bfc7ab4ba6d2be0d046c5c01eb0b274b8e45bb
Conor Timmis <mctto@protonmail.com>
2024-01-09 17:45:02 +0000

    Add EventListener to restart button

--- assets/js/flip.js
+++ assets/js/flip.js
@@ -1,2 +1,8 @@
 /* jshint esversion: 11 */
 
+let restartBtn = document.getElementById("restart-btn");
+restartBtn.addEventListener("click", restart);
+
+function restart() {
+    location.reload();
+}
