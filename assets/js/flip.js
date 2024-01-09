71148f21edae7c2d441179b3cf99873469183d67
Conor Timmis <mctto@protonmail.com>
2024-01-09 17:50:57 +0000

    Add fetch for card data

--- assets/js/flip.js
+++ assets/js/flip.js
@@ -1,8 +1,24 @@
 /* jshint esversion: 11 */
 
+Const cardContainer = document.querySelector(".card-container");
+let cards = [];
+let firstCard, secondCard;
+let lockBoard = false;
+let score = 0;
+
 let restartBtn = document.getElementById("restart-btn");
 restartBtn.addEventListener("click", restart);
 
 function restart() {
     location.reload();
 }
+
+fetch("./assets/data/cards.json");
+ .then((res) => res.json())
+ .then((data) => {
+    cards = [...data, ...data];
+    shuffleCards();
+    generateCards();
+ });
+
+ 
