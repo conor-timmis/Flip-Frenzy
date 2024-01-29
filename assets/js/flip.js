d9bd6831c6fc708898f796fa2baed4397f91ede0
Conor Timmis <mctto@protonmail.com>
2024-01-29 20:00:15 +0000

    Delete Data/Json to opt into data handling in JS

--- assets/js/flip.js
+++ assets/js/flip.js
@@ -1,151 +1,181 @@
 /* jshint esversion: 11 */
 
-const cardContainer = document.getElementById("card-container");
-let cards = [];
-let firstCard, secondCard;
-let lockBoard = false;
-let score = 0;
-let attempts = document.querySelector('.Attempts');
-let totalAttempts = 0;
-
-document.addEventListener('DOMContentLoaded', function () {
-    var howToPlayButton = document.getElementById('htp-button');
-    var howToPlaySection = document.getElementById('howtoplay');
-
+const data = [
+    {
+      image: "../assets/images/card-clubs.png",
+      name: "clubs",
+    },
+    {
+      image: "../assets/images/card-diamonds.png",
+      name: "diamonds",
+    },
+    {
+      image: "../assets/images/card-hearts.png",
+      name: "hearts",
+    },
+    {
+      image: "../assets/images/card-spades.png",
+      name: "spades",
+    },
+    {
+      image: "../assets/images/card-queens.png",
+      name: "queens",
+    },
+    {
+      image: "../assets/images/card-kings.png",
+      name: "kings",
+    },
+  ];
+  
+  const cardContainer = document.getElementById("card-container");
+  let cards = [];
+  let firstCard, secondCard;
+  let lockBoard = false;
+  let score = 0;
+  let attempts = document.querySelector(".Attempts");
+  let totalAttempts = 0;
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    var howToPlayButton = document.getElementById("htp-button");
+    var howToPlaySection = document.getElementById("howtoplay");
+  
     if (howToPlayButton && howToPlaySection) {
-        howToPlayButton.addEventListener('click', function () {
-            howToPlaySection.style.display = (howToPlaySection.style.display === 'block') ? 'none' : 'block';
-        });
+      howToPlayButton.addEventListener("click", function () {
+        howToPlaySection.style.display =
+          howToPlaySection.style.display === "block" ? "none" : "block";
+      });
     }
-});
-
-fetch("./assets/data/cards.json")
-    .then((res) => res.json())
-    .then((data) => {
-        cards = [...data, ...data];
-        shuffleCards();
-        generateCards();
-    });
-
-function shuffleCards() {
+  });
+  
+  cards = [...data, ...data];
+  shuffleCards();
+  generateCards();
+  
+  function shuffleCards() {
     let currentIndex = cards.length,
-        randomIndex,
-        temporaryValue;
+      randomIndex,
+      temporaryValue;
     while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = cards[currentIndex];
-        cards[currentIndex] = cards[randomIndex];
-        cards[randomIndex] = temporaryValue;
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = cards[currentIndex];
+      cards[currentIndex] = cards[randomIndex];
+      cards[randomIndex] = temporaryValue;
     }
-}
-
-function generateCards() {
+  }
+  
+  function generateCards() {
     const maxAllowedOccurrences = 3;
     const cardBackOccurrences = {};
     for (let card of cards) {
-        const cardElement = document.createElement("div");
-        cardElement.classList.add("card");
-        cardElement.setAttribute("data-name", card.name);
-        let randomBack;
-        do {
-            randomBack = Math.floor(Math.random() * 4) + 1;
-        } while (cardBackOccurrences[randomBack] >= maxAllowedOccurrences);
-        cardBackOccurrences[randomBack] = (cardBackOccurrences[randomBack] || 0) + 1;
-        cardElement.innerHTML = `
-            <div class="card-container">
-            <img src="assets/images/card-back${randomBack}.png" alt="random card back" data-card="${card.name}" class="card back">
-                <img src="images/${card.image}" alt="${card.name}" class="card front flipped">
-            </div>
-        `;
-        cardContainer.appendChild(cardElement);
-        cardElement.addEventListener("click", flipCard);
+      const flipCard = document.createElement("div");
+      flipCard.classList.add("flip-card");
+      flipCard.setAttribute("data-name", card.name);
+      let randomBack;
+      do {
+        randomBack = Math.floor(Math.random() * 4) + 1;
+      } while (cardBackOccurrences[randomBack] >= maxAllowedOccurrences);
+  
+      cardBackOccurrences[randomBack] =
+        (cardBackOccurrences[randomBack] || 0) + 1;
+      flipCard.innerHTML = `
+              <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                      <img src="assets/images/card-back${randomBack}.png" alt="random card back" data-card="${card.name}" class="card-image">
+                  </div>
+                  <div class="flip-card-back">
+                      <img src="images/${card.image}" alt="${card.name}" class="card-image">
+                  </div>
+              </div>
+          `;
+      cardContainer.appendChild(flipCard);
+      flipCard.addEventListener("click", flipClickedCard)
     }
-}
-
-function flipCard() {
+  }
+  
+  function flipClickedCard() {
     if (lockBoard || this === firstCard) {
-        return;
+      return;
     }
-
-    this.classList.add("flipped");
-
+  
+    this.style.transform = "rotateY(180deg)"
+  
     if (!firstCard) {
-        firstCard = this;
+      firstCard = this;
     } else {
-        secondCard = this;
-        totalAttempts++;
-        attemptsDisplay();
-        checkForMatch();
+      secondCard = this;
+      totalAttempts++;
+      attemptsDisplay();
+      checkForMatch();
     }
-}
-
-function checkForMatch() {
+  }
+  
+  function checkForMatch() {
     if (lockBoard || !firstCard || !secondCard) {
-        return;
+      return;
     }
-
+  
     let isMatch = firstCard.dataset.name === secondCard.dataset.name;
-
+  
     if (isMatch) {
-        disableCards();
-        score++;
-        document.querySelector(".score").textContent = score;
-        if (score === cards.length / 2) {
-            showWinner();
-            resetScore();
-        }
+      disableCards();
+      score++;
+      document.querySelector(".score").textContent = score;
+      if (score === cards.length / 2) {
+        showWinner();
+        resetScore();
+      }
     } else {
-        lockBoard = true;
-        unflipCards();
+      lockBoard = true;
+      unflipCards();
     }
-}
-
-function attemptsDisplay() {
+  }
+  
+  function attemptsDisplay() {
     attempts.textContent = totalAttempts;
-}
-
-function disableCards() {
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
-
+  }
+  
+  function disableCards() {
+    firstCard.removeEventListener("click", flipClickedCard);
+    secondCard.removeEventListener("click", flipClickedCard);
+  
     resetBoard();
-}
-
-function unflipCards() {
+  }
+  
+  function unflipCards() {
     setTimeout(() => {
-        if (firstCard && secondCard) {
-            firstCard.classList.remove("flipped");
-            secondCard.classList.remove("flipped");
-        }
-        resetBoard();
-        lockBoard = false;
-    }, 250);
-}
-
-function showWinner() {
-    const winner = document.getElementById('winner');
-    winner.classList.remove('hidden');
-}
-
-
-function resetBoard() {
+      if (firstCard && secondCard) {
+        firstCard.style.transform = "rotateY(0deg)"
+        secondCard.style.transform = "rotateY(0deg)"
+      }
+      resetBoard();
+      lockBoard = false;
+    }, 750);
+  }
+  
+  function showWinner() {
+    const winner = document.getElementById("winner");
+    winner.classList.remove("hidden");
+  }
+  
+  function resetBoard() {
     firstCard = null;
     secondCard = null;
     lockBoard = false;
-}
-
-let restartButtonPressed = false;
-
-let restartBtn = document.getElementById("restart-btn");
-restartBtn.addEventListener("click", restart);
-
-function restart() {
+  }
+  
+  let restartButtonPressed = false;
+  
+  let restartBtn = document.getElementById("restart-btn");
+  restartBtn.addEventListener("click", restart);
+  
+  function restart() {
     restartButtonPressed = true;
     location.reload();
-}
-
-function resetScore() {
+  }
+  
+  function resetScore() {
     score = 6;
     document.querySelector(".score").textContent = score;
-}
+  }
+  
